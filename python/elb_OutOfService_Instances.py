af6c11c38ba81359d7e334be311c0f2478919d1a
Mike Chavez <mike.chavez@badfish.co>
2016-05-20 17:53:56 -0700

    set the elb to a variable not hard coded

--- python/elb_OutOfService_Instances.py
+++ python/elb_OutOfService_Instances.py
@@ -3,11 +3,15 @@
 import boto3
 import json
 
+elb  = raw_input("Enter ELB name: ")
+
 client = boto3.client('elb')
+
+
 
 response = client.describe_instance_health(
     ### Modify below to the name of your ELB
-    LoadBalancerName='elb-test-1',
+    LoadBalancerName=elb,
     )
 
 
